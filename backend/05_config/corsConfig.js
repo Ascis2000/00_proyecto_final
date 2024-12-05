5b2d3e6c9d88df659bf7293a3ca47e3ebaaa311c
unknown <ascis2000@gmail.com>
2024-12-05 16:31:16 +0100

    update/config.js

--- backend/05_config/corsConfig.js
+++ backend/05_config/corsConfig.js
@@ -1,6 +1,6 @@
 
 const corsOptions = {
-    origin: process.env.NODE_ENV === 'production' ? 'https://frontend-filmoteca.onrender.com' : 'http://localhost:5173',
+    origin: process.env.NODE_ENV === 'production' ? 'https://00proyectofinalfrontend.netlify.app/' : 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true
