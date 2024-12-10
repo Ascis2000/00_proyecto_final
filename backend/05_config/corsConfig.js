7bdbcb35cc3358cbd85ad3daf7282d50bb00b910
Sergio Ortega <ortegagarrido.sergio@gmail.com>
2024-12-10 18:03:55 +0100

    try_login_deploy

--- backend/05_config/corsConfig.js
+++ backend/05_config/corsConfig.js
@@ -14,7 +14,7 @@ const corsOptions = {
         }
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: 'Content-Type, Authorization',
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
     credentials: true
 };
 
