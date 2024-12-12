780bd8332209930aa782088c23e7c38ff1584f8e
Sergio Ortega <ortegagarrido.sergio@gmail.com>
2024-12-12 17:55:39 +0100

    try_login

--- backend/01_controllers/auth.controller.js
+++ backend/01_controllers/auth.controller.js
@@ -3,8 +3,6 @@ const bcrypt = require('bcryptjs');
 const authModel = require('../02_models_SQL/auth.model');
 const { createToken } = require('../05_config/jsonWebToken');
 
-
-
 const login = async (req, res) => {
     try {
         const { email, password } = req.body;
@@ -13,19 +11,13 @@ const login = async (req, res) => {
         if (user) {
             const token = createToken(user);
 
-            // Condicional para diferenciar el entorno local y de producción
-            const cookieOptions = process.env.NODE_ENV === 'production'
-                ? {
-                    httpOnly: true,
-                    sameSite: 'None', // Permite el envío de cookies entre diferentes dominios en producción
-                    secure: true,     // Requiere HTTPS en producción
-                    path: '/',
-                }
-                : {
-                    httpOnly: true,
-                    sameSite: 'Strict', // Solo permite la cookie en el mismo dominio
-                    path: '/',
-                };
+            // Configurar las opciones de la cookie dependiendo del entorno
+            const cookieOptions = {
+                httpOnly: true, // Mejor mantener esto como true para evitar accesos de JavaScript
+                sameSite: 'None', // Necesario para solicitudes cross-origin
+                secure: true, // Solo usar secure en producción
+                path: '/', // Ruta para la cookie
+            };
 
             res.cookie('token', token, cookieOptions)
                 .status(200)
@@ -37,6 +29,35 @@ const login = async (req, res) => {
         res.status(500).json({ success: false, msg: error.message });
     }
 };
+
+// Funciona en local
+
+// const login = async (req, res) => {
+//     try {
+//         const { email, password } = req.body;
+//         const user = await authModel.login(email, password);
+
+//         if (user) {
+//             const token = createToken(user);
+
+//             const cookieOptions = {
+//                 httpOnly: false, // Permite acceder desde JS
+//                 sameSite: 'None', // Cambiar a None para solicitudes cross-origin
+//                 secure: true, // Necesario si usas HTTPS
+//                 path: '/', // Ruta para la cookie
+//             };
+
+//             res.cookie('token', token, cookieOptions)
+//                 .status(200)
+//                 .json({ success: true, ...user });
+//         } else {
+//             res.status(400).json({ success: false, msg: "Credenciales incorrectas" });
+//         }
+//     } catch (error) {
+//         res.status(500).json({ success: false, msg: error.message });
+//     }
+// };
+
 
 
 function logout(req, res) {
@@ -62,13 +83,13 @@ const mostrarUser = async (req, res) => {
 
         // Decodifica el token (asegúrate de tener la misma clave secreta con la que se generó el token)
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        
+
         // Extrae el id del token
         const userId = decoded.id;
 
         const misAds = await adService.getAllAds();
 
-        res.render('userDashboard', { 
+        res.render('userDashboard', {
             role: 'user',
             userid: userId,
             ads: misAds
@@ -84,7 +105,7 @@ const mostrarAdmin = async (req, res) => {
 
         const misAds = await adService.getAllAds();
 
-        res.render('adminDashboard', { 
+        res.render('adminDashboard', {
             role: 'admin',
             ads: misAds
         });
