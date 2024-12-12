db9658a7b8af19885cb263d99ca80d9a833cfdce
Sergio Ortega <ortegagarrido.sergio@gmail.com>
2024-12-12 18:24:38 +0100

    volvemos atras

--- backend/01_controllers/auth.controller.js
+++ backend/01_controllers/auth.controller.js
@@ -3,35 +3,6 @@ const bcrypt = require('bcryptjs');
 const authModel = require('../02_models_SQL/auth.model');
 const { createToken } = require('../05_config/jsonWebToken');
 
-const login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await authModel.login(email, password);
-
-        if (user) {
-            const token = createToken(user);
-
-            // Configurar las opciones de la cookie dependiendo del entorno
-            const cookieOptions = {
-                httpOnly: true, // Mejor mantener esto como true para evitar accesos de JavaScript
-                sameSite: 'None', // Necesario para solicitudes cross-origin
-                secure: true, // Solo usar secure en producción
-                path: '/', // Ruta para la cookie
-            };
-
-            res.cookie('token', token, cookieOptions)
-                .status(200)
-                .json({ success: true, ...user });
-        } else {
-            res.status(400).json({ success: false, msg: "Credenciales incorrectas" });
-        }
-    } catch (error) {
-        res.status(500).json({ success: false, msg: error.message });
-    }
-};
-
-// Funciona en local
-
 // const login = async (req, res) => {
 //     try {
 //         const { email, password } = req.body;
@@ -40,10 +11,11 @@ const login = async (req, res) => {
 //         if (user) {
 //             const token = createToken(user);
 
+//             // Configurar las opciones de la cookie dependiendo del entorno
 //             const cookieOptions = {
-//                 httpOnly: false, // Permite acceder desde JS
-//                 sameSite: 'None', // Cambiar a None para solicitudes cross-origin
-//                 secure: true, // Necesario si usas HTTPS
+//                 httpOnly: true, // Mejor mantener esto como true para evitar accesos de JavaScript
+//                 sameSite: 'None', // Necesario para solicitudes cross-origin
+//                 secure: true, // Solo usar secure en producción
 //                 path: '/', // Ruta para la cookie
 //             };
 
@@ -57,6 +29,33 @@ const login = async (req, res) => {
 //         res.status(500).json({ success: false, msg: error.message });
 //     }
 // };
+
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await authModel.login(email, password);
+
+        if (user) {
+            const token = createToken(user);
+
+            const cookieOptions = {
+                httpOnly: false, // Permite acceder desde JS
+                sameSite: 'None', // Cambiar a None para solicitudes cross-origin
+                secure: true, // Necesario si usas HTTPS
+                path: '/', // Ruta para la cookie
+            };
+
+            res.cookie('token', token, cookieOptions)
+                .status(200)
+                .json({ success: true, ...user });
+        } else {
+            res.status(400).json({ success: false, msg: "Credenciales incorrectas" });
+        }
+    } catch (error) {
+        res.status(500).json({ success: false, msg: error.message });
+    }
+};
 
 
 
